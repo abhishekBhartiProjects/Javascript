17bea2ee2a45515903b5c4b2bd453f59efef0661
abhishekbharti <abhishek.bharti@testbook.com>
2018-02-07 10:54:21 +0530

    calling different function on different url request by user - connect example

--- movie/server.js
+++ movie/server.js
@@ -4,18 +4,16 @@ var http = require('http');
 
 var app = connect();
 
-function doFirst(request, response, next) {
-    console.log("this is first function");
-    next();
+function profile(request, response) {
+    console.log("User requested profile");
 }
 
-function doSecond(request, response, next) {
-    console.log("this is second function");
-    next();
+function forum(request, response) {
+    console.log("User requested forum");
 }
 
 
-app.use(doFirst);
-app.use(doSecond);
+app.use('/profile', profile);
+app.use('/forum', forum);
 
 http.createServer(app).listen(8888);
